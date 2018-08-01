793383674985416ad1b932b87f64f733d86f8a4f
jan nguyen <jan.nguyen@me.com>
2018-08-02 00:01:07 +0200

    feat: stop function

--- js/app.js
+++ js/app.js
@@ -102,6 +102,7 @@ allCards.forEach(function(card) {
     });   
 });     
     
+//Function for counting clicks - not working
 let clicks = 0;
 
 document.onclick = function() {
@@ -112,10 +113,15 @@ document.onclick = function() {
 
 
 //Timer function
+let sec = 0;
 
-var sec = 0;
 function pad ( val ) { return val > 9 ? val : "0" + val; }
-setInterval( function(){
+let interval = setInterval( function(){
     document.getElementById("seconds").innerHTML=pad(++sec%60);
     document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
 }, 1000);
+
+//Stop timer function
+function stopTimer() {
+    clearInterval(interval);
+};
