cd4dc688b06651d545f877ae748422f20b91b941
jan nguyen <jan.nguyen@me.com>
2018-08-01 21:58:27 +0200

    feat: add timer

--- js/app.js
+++ js/app.js
@@ -102,13 +102,20 @@ allCards.forEach(function(card) {
     });   
 });     
     
+let clicks = 0;
+
+document.onclick = function() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+    
+};
 
 
-//Flipped cards function
+//Timer function
 
-
-//Flipping card when click
-
-
-
-
+var sec = 0;
+function pad ( val ) { return val > 9 ? val : "0" + val; }
+setInterval( function(){
+    document.getElementById("seconds").innerHTML=pad(++sec%60);
+    document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+}, 1000);
