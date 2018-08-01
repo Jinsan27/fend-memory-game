a3c4c36156bd261d59b822bd0822bc1a5480e694
jan nguyen <jan.nguyen@me.com>
2018-08-02 00:10:18 +0200

    refact: rewrote timer function more clear

--- js/app.js
+++ js/app.js
@@ -115,10 +115,18 @@ document.onclick = function() {
 //Timer function
 let sec = 0;
 
-function pad ( val ) { return val > 9 ? val : "0" + val; }
-let interval = setInterval( function(){
-    document.getElementById("seconds").innerHTML=pad(++sec%60);
-    document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+function format( val ) {
+    return val > 9 ? val : "0" + val
+}
+
+const interval = setInterval(() => {
+    sec += 1;
+    const seconds = sec % 60;
+    const mins = Math.floor(sec / 60);
+
+    document.getElementById("seconds").innerHTML=format(seconds)
+    document.getElementById("minutes").innerHTML=format(mins)
+    
 }, 1000);
 
 //Stop timer function
